f9ce75788d0405e6ba48f35af63de09036fc05fd
Prakhya <prakhya@Prakhyas-MacBook-Air.local>
2026-08-02 18:25:24 +0530

    refactor: rename users route to user
    - Renamed users.js to user.js for consistency
    - Updated imports accordingly

--- prakhya_ecomly_server/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/users');
-
-router.get('/',userController.getUsers);
-router.get('/:id',userController.getUserById);
-router.put('/:id',userController.updateUser)
-
-module.exports = router;
