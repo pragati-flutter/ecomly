1afe7d75cc43b06027d0ca18e79170eaf2a183ee
Prakhya <prakhya@Prakhyas-MacBook-Air.local>
2026-08-02 18:25:05 +0530

    feat: add category controllers
    - Implemented getCategories and getCategoryById
    - Added basic error handling for category APIs

---
+++ prakhya_ecomly_server/controllers/categories.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.router;
+const {Category} = require('../models/category');
+
+const getCategories = async function(req,res){
+    try{
+     const categories = await Category.find();
+
+     if(!categories){
+        return res.json({
+            message: 'Category does not found'
+        });
+     }
+
+     return res.json(categories);
+     
+    }catch(error){
+        console.error(error);
+        return res.json({type: error.type, message:error.message});
+
+
+    }
+
+}
+
+const getCategoryById = async function(req,res){
+    try{
+      const category = await Category.findById(req.params.id);
+      if(!category){
+        return res.json({message:'Category no found'});
+      }
+      return res.json(category);
+
+    }catch(error){
+        console.error(error);
+        return res.json({type:error.type,message:error.message});
+    }
+}
+
+
