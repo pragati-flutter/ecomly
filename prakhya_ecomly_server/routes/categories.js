a5d597fa7e02d82239b278aa753d9dcc14c37200
Prakhya <prakhya@Prakhyas-MacBook-Air.local>
2026-08-02 18:25:11 +0530

    feat: add category routes
    - Connected category controller with routes
    - Added endpoints for fetching categories and category by id
    - Fixed Router initialization and duplicate declarations

---
+++ prakhya_ecomly_server/routes/categories.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router;
+const categoryController = require('../controllers/categories');
+const router = require('./products');
+
+router.get('/',categoryController.getCategories);
+router.get('/:id',categoryController.getCategoryById);
+
+module.exports = router;
+
+
