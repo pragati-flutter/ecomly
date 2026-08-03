4bd49b805c80a3ad67d7110e8faf2e55a0792516
Prakhya <prakhya@Prakhyas-MacBook-Air.local>
2026-08-03 08:08:11 +0530

    feat(products): implement getProducts with filtering, criteria and pagination

--- prakhya_ecomly_server/controllers/products.js
+++ prakhya_ecomly_server/controllers/products.js
@@ -1,2 +1,53 @@
-exports.getProductsCount = function(req,res){}
-exports.getProductDetails = (req,res)=>{}
+const { query } = require("express-validator");
+const { Product } = require("../models/product");
+
+const getProducts = async function (req, res) {
+  try {
+    let products;
+    const page = req.querry.page || 1;
+    const pageSize = 10;
+
+    if (req.query.criteria) {
+      let query = {};
+      if (req.query.category) {
+        query["category"] = req.query.category;
+      }
+
+      switch (req.query.criteria) {
+        case "newArrivals": {
+          const twoWeekAgo = new Date();
+          twoWeekAgo.setDate(twoWeekAgo.getDate() - 14);
+          query["dateAdded"] = { $gte: twoWeekAgo };
+          break;
+        }
+        case "popular": {
+          query["rating"] = { $gte: 4.5 };
+          break;
+        }
+        default:
+          break;
+      }
+
+      products = await Product.find(query)
+        .select("-image -reviews -size")
+        .skip((page - 1) * pageSize)
+        .limit(pageSize);
+    } else if ((req, query.category)) {
+      products = await Product.find(query)
+        .select("-image -reviews -size")
+        .skip((page - 1) * pageSize)
+        .limit(pageSize);
+    } else {
+      products = await Product.select("-image -reviews -size").skip(
+        (page - 1) * pageSize,
+      );
+    }
+    if(!products){
+        return res.status(404).json({message:'product does not exist'});
+    }
+     return res.json(products);
+  } catch (error) {
+    console.error(error);
+    return res.json({ type: error.type, message: error.message });
+  }
+};
--- prakhya_ecomly_server/routes/products.js
+++ prakhya_ecomly_server/routes/products.js
@@ -1,9 +1,16 @@
 const express = require('express');
 
+
 const router = express.Router();
-const productControllers = require('../controllers/products')
-router.get('/products/counts',productControllers.getProductsCount);
-router.get('/products/:id',);
-router.delete('/products/:id',);
-router.put('/products/:id',);
+const productController = require('../controllers/products');
+const reviewsController = require('../controllers/reviews');
+
+
+
+router.get('/',productController.getProducts);
+router.get('/search',productController.searchProducts);
+router.post('/:id',productController.getProductById);
+router.get('/:id/reviews',reviewController.leaveReview);
+router.get('/:id/reviews',reviewController.getProductReviews);
+
 module.exports = router;
