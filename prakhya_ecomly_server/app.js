6e85b8e6b571a15294883550bb17586308c21eb9
Prakhya <prakhya@Prakhyas-MacBook-Air.local>
2026-08-03 08:07:48 +0530

    fix(app): correct app configuration and middleware setup

--- prakhya_ecomly_server/app.js
+++ prakhya_ecomly_server/app.js
@@ -2,38 +2,42 @@ console.log("Jai Shree Ram");
 
 const express = require("express");
 const app = express();
-
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 require("dotenv/config");
-
 const authJwt = require('./middleware/jwt');
 const errorHandler = require('./middleware/error_handler');
+
+
+
 const authRouter = require('./routes/auth');
 const userRouter = require('./routes/users');
 const adminRouter = require('./routes/admin');
 const categoriesRouter = require('./routes/categories');
+const productRouter = require('./routes/products');
 
 const env = process.env;
 const API = env.API_URL;
 
 // Middleware
 app.use(cors());
+app.options('*',cors());
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
-
 app.use(authJwt());
+app.use(errorHandler); 
+
 
 // Routes
 app.use(`${API}/`, authRouter);
 app.use(`${API}/users`,userRouter);
 app.use(`${API}/admin`,adminRouter);
 app.use(`${API}/categories`,categoriesRouter);
+app.use(`${API}/products`,productRouter)
 app.use('/public',express.static(__dirname + '/public'));
 
-app.use(errorHandler); 
 
 
 
